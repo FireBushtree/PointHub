2bd6d3d15c67b26e78b9cc090658c017c1a50a2f
firebushtree <firebushtree@163.com>
2025-09-22 17:55:48 +0800

    feat: update grid config

--- src/pages/ClassStudents.tsx
+++ src/pages/ClassStudents.tsx
@@ -557,7 +557,7 @@ export default function ClassStudents() {
                     </div>
                   </div>
 
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 [2880px]:grid-cols-8 gap-3">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 2xl:grid-cols-6 gap-3">
                     {filteredStudents.map(student => (
                       <div
                         key={student.id}
